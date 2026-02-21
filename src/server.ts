d6f869e34a91b5d161e694303fca673579ba8795
osmanzakaria532 <osmanzakaria179@gmail.com>
2026-02-22 05:23:56 +0600

    post and get api added for todos

--- src/server.ts
+++ src/server.ts
@@ -172,6 +172,47 @@ app.delete('/users/:id', async (req: Request, res: Response) => {
   }
 });
 
+// todos
+
+app.post('/todos', async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+      [user_id, title],
+    );
+    res.status(201).json({
+      success: true,
+      message: 'todo created',
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.get('/todos', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+
+    res.status(200).json({
+      success: true,
+      message: 'Todos retrieved Successfully',
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      details: error,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
