e6d17d94210ba9e73723e24adfdac134068ec442
osmanzakaria532 <osmanzakaria179@gmail.com>
2026-02-22 04:00:06 +0600

    put and delete api added for users

--- src/server.ts
+++ src/server.ts
@@ -119,6 +119,59 @@ app.get('/users/:id', async (req: Request, res: Response) => {
   }
 });
 
+app.put('/users/:id', async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      `
+      UPDATE users SET name=$1, email=$2 WHERE id=$3  RETURNING *`,
+      [name, email, req.params.id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'User Not Found',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'User updated Successfully',
+        data: result.rows[0],
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.delete('/users/:id', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'User Not Found',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'User delete Successfully',
+        data: null,
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
