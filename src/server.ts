39994251c752c19864f55ef159a5387bb0d6423d
osmanzakaria532 <osmanzakaria179@gmail.com>
2026-02-22 19:47:32 +0600

    saparate db and configaration file in config folder

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,14 @@
-import dotenv from 'dotenv';
 import express, { NextFunction, Request, Response } from 'express';
-import path from 'path';
-import { Pool } from 'pg';
+import config from './config';
+import intiDB, { pool } from './config/db';
 
-dotenv.config({ path: path.join(process.cwd(), '.env') });
 const app = express();
-const port = 5000;
+const port = config.port;
 
 // Parser
 app.use(express.json());
 
-// DB
-const pool = new Pool({
-  connectionString: `${process.env.Connection_String}`,
-});
-
-const intiDB = async () => {
-  await pool.query(
-    `
-    CREATE TABLE IF NOT EXISTS users(
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      email VARCHAR(150) UNIQUE NOT NULL,
-      age INT,
-      phone VARCHAR(15),
-      address TEXT,
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-    )
-    `,
-  );
-
-  await pool.query(
-    `
-      CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-      )
-    `,
-  );
-};
-
+// initialzing DB
 intiDB();
 
 // logger middleware
